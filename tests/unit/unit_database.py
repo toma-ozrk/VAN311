d7c2b7c7025741bab7d3999e922ee652d23cb0e1
Toma Ozarchevici <toa17@sfu.ca>
2025-11-21 16:32:43 -0800

    feat(test): add unit test for upsert db

--- tests/unit/unit_database.py
+++ tests/unit/unit_database.py
@@ -1,8 +1,9 @@
 import sqlite3
+from unittest.mock import Mock, patch
 
 import pytest
 
-from van311.database import get_db_connection
+from van311.database import get_db_connection, upsert_service_requests
 
 
 def test_get_db_connection():
@@ -23,3 +24,31 @@ def test_get_db_connection():
 
     finally:
         con.close()
+
+
+MOCK_SUCCESS_DATA = [{"request_id": "1"}, {"value": "coolvalue"}]
+EXPECTED_SQL = """INSERT INTO service_requests VALUES (?, ?)
+   ON CONFLICT(id) DO UPDATE SET value = EXCLUDED.value"""
+
+
+@patch("van311.database.get_db_connection")
+def test_upsert_service_requests(mock_get_conn):
+    mock_conn = Mock()
+    mock_cursor = Mock()
+
+    mock_conn.cursor.return_value = mock_cursor
+
+    upsert_service_requests(mock_conn, MOCK_SUCCESS_DATA)
+
+    mock_conn.assert_called_once()
+    mock_cursor.execute.assert_called_once()
+
+    call_args, call_kwargs = mock_cursor.execute.call_args
+    actual_sql = call_args[0]
+    actual_params = call_args[1]
+
+    assert actual_sql.starts_with("""INSERT INTO service_requests""")
+    assert actual_params == ("1", "coolvalue")
+
+    mock_conn.commit.assert_called_once()
+    mock_conn.close.assert_called_once()
