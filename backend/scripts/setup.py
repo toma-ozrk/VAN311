6dfb4e07f2a9198cbfa95c4b3cb23a3227d7d035
Toma Ozarchevici <toa17@sfu.ca>
2025-11-26 04:29:40 -0800

    refactor setup.py

--- backend/scripts/setup.py
+++ backend/scripts/setup.py
@@ -1,5 +1,5 @@
-from van311.db.seeding import seed_database
 from van311.db.metrics import calculate_metrics
+from van311.db.seeding import seed_database
 
 if __name__ == "__main__":
     seed_database()
