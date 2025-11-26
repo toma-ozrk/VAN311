53b67f90d108b5e33b170a189720124a84d9e429
Toma Ozarchevici <toa17@sfu.ca>
2025-11-25 22:18:51 -0800

    add time operations for db logging

--- backend/src/van311/utils/dates.py
+++ backend/src/van311/utils/dates.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, datetime, time
 
 from dateutil.relativedelta import relativedelta
 
@@ -8,3 +8,27 @@ def subtract_months_from_today(n):
     month = dt.month
     year = dt.year
     return (month, year)
+
+def calculate_day_difference(open_t: str, close_t: str):
+    open_t_date = date.fromisoformat(open_t.split("T")[0])
+    close_t_date = date.fromisoformat(close_t)
+
+    return (close_t_date - open_t_date).days
+
+def timestamp_to_date_obj(ts):
+    time_split = ts.split("T")
+    ts_date = date.fromisoformat(time_split[0])
+    ts_time = time.fromisoformat(time_split[1].split("+")[0])
+    return datetime.combine(ts_date, ts_time)
+
+def calculate_timestamp_difference(open_t: str, mod_t: str):
+    open_time = timestamp_to_date_obj(open_t)
+    mod_time = timestamp_to_date_obj(mod_t)
+
+    time_difference = (mod_time - open_time)
+    days_elapsed: float = time_difference.days + time_difference.seconds / 86400
+
+    return days_elapsed.__format__(".2f")
+
+if __name__ == "__main__":
+    pass
