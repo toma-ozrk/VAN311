6845a245aa665718bbcfb7f88dd92558f02530b4
Toma Ozarchevici <toa17@sfu.ca>
2025-11-26 20:08:26 -0800

    implement metrics testing

--- backend/tests/conftest.py
+++ backend/tests/conftest.py
@@ -1,6 +1,9 @@
+import sqlite3
 from unittest.mock import MagicMock
 
 import pytest
+
+from van311.db.metrics import create_db_metrics_table
 
 
 @pytest.fixture
@@ -17,3 +20,33 @@ def database_mocks():
         "pbar_instance": mock_pbar_yielded,
         "tqdm_manager": mock_tqdm_context_manager,
     }
+
+
+@pytest.fixture(scope="function")
+def db_metrics_connection():
+    inserted_rows = [
+        # la           it      ttr ttu close_ts
+        ("Kitsilano", "Pothole", 4, 2, 1),
+        ("Kitsilano", "Graffiti", 4, 2, 1),
+        ("Downtown", "Pothole", 2, 1, 1),
+        ("Downtown", "Graffiti", None, 2, None),
+        (None, "Other", 1, 1, 1),
+    ]
+
+    con = sqlite3.connect(":memory:")
+    con.row_factory = sqlite3.Row
+
+    con.execute(
+        """CREATE TABLE service_requests(local_area TEXT, issue_type TEXT,
+        time_to_resolve INT, time_to_update REAL, close_ts INT)"""
+    )
+
+    create_db_metrics_table(con)
+
+    for row in inserted_rows:
+        con.execute("""INSERT INTO service_requests VALUES (?, ?, ?, ?, ?)""", row)
+
+    con.commit()
+    yield con
+
+    con.close()
